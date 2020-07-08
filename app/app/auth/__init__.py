f54acb867de1e38e3d9f19992450ee3097e0aa38
peppato <cfcardillo23@gmail.com>
2020-07-08 09:11:21 -0400

    verify_password moved to auth init

--- app/app/auth/__init__.py
+++ app/app/auth/__init__.py
@@ -1,5 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, g
+from app.models import User
+from app import auth_manager
 
 bp = Blueprint('auth', __name__)
+
+@auth_manager.verify_password
+def verify_password(username_or_token, password):
+    user = User.verify_auth_token(username_or_token)
+    if user:
+        verified = True
+        g.user = user
+    else:
+        user = User.query.filter_by(username = username_or_token.lower().replace(" ", "")).first()
+        if user and user.check_password(password):
+            verified = True
+            g.user = user
+        else:
+            verified = False
+    return verified
 
 from app.auth import routes
--- app/app/auth/routes.py
+++ app/app/auth/routes.py
@@ -1,31 +1,12 @@
 from . import bp
-import subprocess
-from app.models import User
 from app import auth_manager
 from flask import jsonify, request, g
-from flask_restless import ProcessingException
-
-@auth_manager.verify_password
-def verify_password(username_or_token, password):
-    user = User.verify_auth_token(username_or_token)
-    if user:
-        verified = True
-        g.user = user
-    else:
-        user = User.query.filter_by(username = username_or_token.lower().replace(" ", "")).first()
-        if user and user.check_password(password):
-            verified = True
-            g.user = user
-        else:
-            verified = False
-    return verified
 
 @bp.route('/token', methods = ['GET'])
 @auth_manager.login_required
 def return_token():
     token = g.user.generate_auth_token()
     return jsonify({ 'token': token.decode('ascii') })
-
 
 @bp.route('/secure', methods = ['GET'])
 @auth_manager.login_required
