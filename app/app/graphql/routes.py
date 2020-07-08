bf72161b25592275d9a344ab59acc6c6a6cae355
peppato <cfcardillo23@gmail.com>
2020-07-08 09:27:39 -0400

    GraphQL endpoint requires auth!

--- app/app/graphql/routes.py
+++ app/app/graphql/routes.py
@@ -1,12 +1,17 @@
 from .schema import schema
 from flask_graphql import GraphQLView
+from app import auth_manager
 
 from . import bp
 
+def graphql_view():
+    view = GraphQLView.as_view(
+        'graphql',
+        schema=schema,
+        graphiql=True
+    )
+    return auth_manager.login_required(view)
+
 bp.add_url_rule('/',
-                 view_func=GraphQLView.as_view(
-                     'graphql',
-                     schema=schema,
-                     graphiql=True
-                 )
+                view_func=graphql_view()
 )
