0f22fce4edb169306238f92ea13d4bc69bb6f600
peppato <cfcardillo23@gmail.com>
2020-07-09 23:37:40 -0400

    auth_manager.current_user(); auth_manager.error_handler; more extensible

--- app/app/auth/__init__.py
+++ app/app/auth/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, g, session
+from flask import Blueprint, session
 from app.models import User
 from app import auth_manager
 
@@ -8,18 +8,21 @@ bp = Blueprint('auth', __name__)
 def verify_password(username_or_token, password):
     user = User.verify_auth_token(username_or_token)
     if user:
-        verified = True
-        g.user = user
+        verified = user
         session['test'] = 'carrots'
         # is it okay to put the auth token in the session object?
         # then check the session for the token before we check what the user sent?
     else:
         user = User.query.filter_by(username = username_or_token.lower().replace(" ", "")).first()
         if user and user.check_password(password):
-            verified = True
-            g.user = user
+            verified = user
         else:
             verified = False
     return verified
+
+@auth_manager.error_handler
+def auth_error(status):
+    return "Please pass a valid username:password or a valid token:anytext using basic access authentication", status
+    # if you can think of more error messages, you can pass them in via g, e.g. g.auth_manager_error_message
 
 from app.auth import routes
--- app/app/auth/routes.py
+++ app/app/auth/routes.py
@@ -1,15 +1,16 @@
 from . import bp
 from app import auth_manager
-from flask import jsonify, request, g, session
+from flask import jsonify, request, session
 
 @bp.route('/token', methods = ['GET'])
 @auth_manager.login_required
 def return_token():
-    token = g.user.generate_auth_token()
+    token = auth_manager.current_user().generate_auth_token()
     # is it okay to put the auth token in the session object?
     return jsonify({ 'token': token.decode('ascii') })
 
 @bp.route('/secure', methods = ['GET'])
 @auth_manager.login_required
 def hello_secure():
-    return jsonify({'message': 'hello ' + g.user.username + '! The secret message is ' + session['test']})
+    current_user = auth_manager.current_user()
+    return jsonify({'message': 'hello ' + current_user.username + '! The secret message is ' + session['test']})
