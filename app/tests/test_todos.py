6a69300a22ffeeba4504949c835fcd6879e43546
peppato <cfcardillo23@gmail.com>
2020-07-16 21:31:05 -0400

    Unauthorized todo test

--- app/tests/test_todos.py
+++ app/tests/test_todos.py
@@ -3,7 +3,7 @@ from tests import client
 from tests.utils.user import generate_user
 from tests.utils.graphql import create_todo
 from tests.utils.requests import gql_post, json_response
-from tests.utils.response_checks import check_success_message
+from tests.utils.response_checks import check_success_message, check_error_message
 
 @pytest.fixture(scope="module")
 def todo_user(client):
@@ -15,3 +15,10 @@ def test_todo_creation(client, todo_user):
                         headers=todo_user['token_header'],
                         title="Write some tests")
     check_success_message(response, "Write some tests")
+
+def test_unauthorized_todo(client):
+    response = gql_post(client,
+                        create_todo,
+                        title="Write some tests")
+    # This is janky, should be check error...
+    check_success_message(response, "None")
